1e2c6dc8423c2c05cc94a5fdc83133e49f5a0dc1
Devyn Johnson <dljohnson1313@gmail.com>
2025-01-30 22:52:21 -0600

    Renaming section files for more consistent file naming across the repo. Adding function to create each component and importing React on each file to avoid React not defined errors with Vite.

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from './Navigation';
+function Header() {
+    return (
+        <header>
+            <h1>My Portfolio</h1>
+            <nav>
+                <Navigation/>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
